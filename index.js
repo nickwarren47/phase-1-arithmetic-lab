872a571ddc0b3c62d039a7d4d194c764e9e51737
Nick Warren <nickwarren47@gmail.com>
2022-07-02 18:04:54 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// function multiply (num1, num2){
+//     const answer = num1 * num2
+//     return answer;
+// };
+// multiply (31,2);
+
+// function multiply (num1, num2){
+//     return num1*num2;
+// }
+// multiply (31,2);
+
+let num1 = 2;
+let num2 = 31;
+var multiply = (num1*num2);
+
+var random = Math.floor(Math.random()*10);
+
+let num3 = 16;
+let num4 = 6;
+var mod = (num3 % num4);
+
+var max = Math.max(1,2,3,20);
 
 // Write your code here
